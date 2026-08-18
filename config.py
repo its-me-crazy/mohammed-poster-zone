3cb1106ba4b416486f7d8d248c397d53b3bb2eec
its-me-crazy <itsmecrazy3016day@gmail.com>
2026-08-18 18:55:10 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,70 @@
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+TMDB_API_KEY = os.getenv("TMDB_API_KEY", "").strip()
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+PORT = int(os.getenv("PORT", "10000"))
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+BOT_NAME = os.getenv(
+    "BOT_NAME",
+    "Mohammed Poster Zone"
+)
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+TMDB_LANGUAGE = os.getenv(
+    "TMDB_LANGUAGE",
+    "en-US"
+)
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+REQUEST_TIMEOUT = int(
+    os.getenv("REQUEST_TIMEOUT", "20")
+)
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+if not BOT_TOKEN:
+    raise RuntimeError(
+        "BOT_TOKEN environment variable is missing."
+    )
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+if not TMDB_API_KEY:
+    raise RuntimeError(
+        "TMDB_API_KEY environment variable is missing."
+    )
+
+# ------------------------- #
+# Don't Remove Credit
+# Owner @Mr_Mohammed_29
+# ------------------------- #
